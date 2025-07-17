2fd2bf92f46019def35fc47254f81e456f8b981d
Angie C <a.cajusol98@gmail.com>
2025-07-17 21:01:30 +0200

    Font

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,17 @@ import logoBig from "./assets/logo-big.png";
 
 function App() {
   return (
-    <div className="min-h-screen bg-white text-dark font-sans px-6 py-10 flex flex-col items-center">
+    <div className="min-h-screen bg-white text-dark px-6 py-10 flex flex-col items-center font-lexend">
       {/* Header */}
       <header className="mb-8 text-center">
         <img src={logoBig} alt="" />
-        <p className="text-gray-400 mt-2">Stay aware. Stay focused. Reflect better.</p>
       </header>
 
       {/* Main Grid */}
       <main className="max-w-4xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8">
         {/* Timer Section */}
         <section className="bg-gray-800 p-6 rounded-2xl shadow-md">
-          <h2 className="text-xl font-semibold mb-4">Focus Session</h2>
+          <h2 className="text-xl font-semibold mb-4 text-white">Focus Session</h2>
           <Timer />
         </section>
 
