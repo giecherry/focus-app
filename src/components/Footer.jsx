d82938dadf383a5bfa10a4fa18d5be56b2e87824
Angie C <a.cajusol98@gmail.com>
2026-03-02 23:21:33 +0100

    logos styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import logoNotext from "/logo-no-text.png";
 
 function Footer() {
   return (
-    <footer className="mt-10 text-center text-gray-500 text-sm">
-      BeeFocused — Build focus habits, one session at a time.
+    <footer className="mt-4 text-center text-gray-500 text-sm">
+      <span className="flex items-end justify-center">BeeFocused — Build focus habits, one session at a time<img src={logoNotext} alt="BeeFocused Logo" className="h-8 w-auto object-contain mx-auto" /></span>
     </footer>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,20 @@
 import logoBig from "/logo-big.png";
+import logo1x1 from "/logo-1x1.png";
+
 
 function Header() {
   return (
-    <header className="mb-8 text-center">
-      <img src={logoBig} alt="BeeFocused Logo" />
+    <header className="mb-12 text-center">
+      <img
+        className="hidden sm:block h-20 w-auto object-contain mx-auto"
+        src={logoBig}
+        alt="BeeFocused Logo"
+      />
+      <img
+        className="block sm:hidden h-60 w-auto object-contain mx-auto"
+        src={logo1x1}
+        alt="BeeFocused Logo"
+      />
     </header>
   );
 }
